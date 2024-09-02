61b8ff29059e7ff4982232cd69ef277b05a8c317
Hrach Alaverdyan <halaverd02@gmail.com>
2024-09-02 15:05:59 +0400

    uptated code in async/await and writes fs.writefile with promises,and delete tasks.json

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
 import fetch from "node-fetch";
-import fs from "fs"
-import dotenv from "dotenv"
+import fs from "fs";
+import dotenv from "dotenv";
 
-dotenv.config()
-const acces_token = process.env.ACCES_TOKEN
+dotenv.config();
+const acces_token = process.env.ACCES_TOKEN;
 const url = 'https://www.wrike.com/api/v4/tasks';
 
 const mapTask = (task) => ({
     id: task.id,
     name: task.title,
-    assignee: task.accountId, 
+    assignee: task.accountId,
     status: task.importance,
-    collections: task.parentIds || [], 
+    collections: task.parentIds || [],
     created_at: task.createdDate,
     updated_at: task.updatedDate,
     ticket_url: task.permalink
-});     
+});
 
 
-fetch(url, {
-    method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${acces_token}`,
-        'Accept': 'application/json'
-    }
-})
-.then(response => response.json())
-.then(  
-        data => {
+function writeFileAsync  (filename, data)  {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, data, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+                console.log('Tasks successfully written to tasks.json');
+            }
+        });
+    });
+};
+
+async function fetchAndWriteTasks() {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${acces_token}`,
+                'Accept': 'application/json'
+            }
+        });
+
+        const data = await response.json();
         const tasks = data.data;
         console.log(tasks);
+        const mappedTasks = tasks.map(mapTask);
+        await writeFileAsync('tasks.json', JSON.stringify(mappedTasks, null, 2));
         
-        const mappedTasks = tasks.map(mapTask); 
-        return mappedTasks
-        
-}).then(mappedTasks=>{
-    fs.writeFile('tasks.json', JSON.stringify(mappedTasks, null, 2), (err) => { 
-        if (err) {
-            console.error('Error writing to file:');
-        } else {
-            console.log('Tasks successfully written to tasks.json');
-        }
-    });
-}).catch(err => console.log(err));
+
+    } catch (err) {
+        console.error('Error: ', err);
+    }
+}
+
+fetchAndWriteTasks();
